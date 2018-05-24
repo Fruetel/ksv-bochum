03462bb8ac765c87f3c68c75de5d01610e0e0f80
Thomas Fruetel <github@fruetel.de>
2018-05-24 14:59:43 +0200

    Add sidebar to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,14 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 
 import { rhythm, scale } from '../utils/typography'
-import { Container, Headline } from './styles.js'
+import {
+  Container,
+  Content,
+  Section,
+  Header,
+  Title,
+} from '../assets/styles/styles.js'
+import SideBar from '../components/side-bar/index'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -15,49 +22,54 @@ class BlogPostTemplate extends React.Component {
     return (
       <Container>
         <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-        <Headline>{post.frontmatter.title}</Headline>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
+        <SideBar />
+        <Content>
+          <Section>
+            <Header>{post.frontmatter.title}</Header>
+            <p
+              style={{
+                ...scale(-1 / 5),
+                display: 'block',
+                marginBottom: rhythm(1),
+                marginTop: rhythm(-1),
+              }}
+            >
+              {post.frontmatter.date}
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            <hr
+              style={{
+                marginBottom: rhythm(1),
+              }}
+            />
 
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-          }}
-        >
-          {previous && (
-            <li>
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            </li>
-          )}
+            <ul
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                listStyle: 'none',
+                padding: 0,
+              }}
+            >
+              {previous && (
+                <li>
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                </li>
+              )}
 
-          {next && (
-            <li>
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            </li>
-          )}
-        </ul>
+              {next && (
+                <li>
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </Section>
+        </Content>
       </Container>
     )
   }
